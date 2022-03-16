07af2fe6a5557bea72e1a1326117de955ea3bc5c
Tony Schwebach <tony.schwebach@gmail.com>
2022-03-15 22:15:49 -0400

    test classes

---
+++ tech-news-java-api/src/main/java/com/technews/VariablesPractice.java
@@ -0,0 +1,12 @@
+package com.technews;
+
+public class VariablesPractice {
+    public static void main(String[] args){
+        int firstNumber = 3;
+        double secondNumber = 3.14;
+        double sum = firstNumber + secondNumber;
+        System.out.println(sum);
+        String myString = "5";
+        System.out.println(sum+myString);
+    }
+}
